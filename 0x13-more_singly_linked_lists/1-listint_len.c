2ebe2736eb6599e3ec9e546b49262331efb2b75e
Medinat Isah <soniaapex12@gmail.com>
2023-04-04 16:37:28 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_lent command is to count numbers of elements in a linked listint_t
+ * @h:-this is the List to check
+ * Return: number of elements in list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number_of_elements = 0;
+	const listint_t *p = h;
+
+	while (p != NULL)
+	{
+		number_of_elements++;
+		p = p->next;
+	}
+	return (number_of_elements);
+}
